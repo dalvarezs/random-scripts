f188f3a79fcfb53e36c0107025483298f316915a
dalvarezs <dalvarezs@users.noreply.github.com>
2017-10-13 18:50:25 +0200

    Accepts network ranges with CIDR format

--- ip_in_range.py
+++ ip_in_range.py
@@ -6,17 +6,13 @@
 from argparse import ArgumentParser
 import os
 import sys
-from math import pow
+from netaddr import IPNetwork, IPAddress
+
 
 __author__ = 'David Alvarez @dalvarez_s'
-__version__ = '0.1'
+__version__ = '0.2'
 __doc__ = 'Look IPs up in the Range. ' \
           'UseCase: Useful for identifying IPs not included in the scope of the Vulnerability scan'
-
-def ip_to_num(ip):
-    nip = ip.split(".")
-    return int(nip[0])*pow(256,3)+int(nip[1])*pow(256,2)+int(nip[2])*pow(256,1)+int(nip[3])
-
 
 def rangeCIDR_to_list(range):
     if "-" in range:
@@ -31,7 +27,7 @@ parser = ArgumentParser(
 
 parser.add_argument('-l', '--list', choices=['in','out'], help='show IPs [in/out] of the range', required=True)
 parser.add_argument('ip_file', help='IPs to be searched')
-parser.add_argument('range_file', help='look IPs up in the Range')
+parser.add_argument('range_file', help='look IPs up in the Range. Format accepted: 192.168.1.0-192.168.1.255 / 192.168.1.0/24')
 parser.add_argument('--version', action='version', version=__version__)
 
 
@@ -53,17 +49,24 @@ with open(filename, 'r') as f:
         ip_found = False
         for net_range in ip_range_list:
             # print (net_range)
-            ip_min_max = net_range.strip().split("-")
+            if "-" in net_range:
+                ip_min_max = net_range.strip().split("-") #### function
 
-            if (ip_to_num(ip_min_max[1]) >= ip_to_num(ip)) and (ip_to_num(ip_min_max[0]) <= ip_to_num(ip)):
-                #if args.list == 'in': print (str(ip)+" IP in the range "+str(ip_min_max))
-                in_range.append(ip)
-                ip_found = True
-                break
+                if (IPAddress(ip_min_max[1]) >= IPAddress(ip)) and (IPAddress(ip_min_max[0]) <= IPAddress(ip)):
+                    #if args.list == 'in': print (str(ip)+" IP in the range "+str(ip_min_max))
+                    in_range.append(ip)
+                    ip_found = True
+                    break
+            elif "/" in net_range:
+                if IPAddress(ip) in IPNetwork(net_range):
+                    in_range.append(ip)
+                    ip_found = True
+                    break
+
         if ip_found == False:
             out_range.append(ip)
 
 if args.list == 'in':
-    for i in in_range: print (i)
+    for i in in_range: print i
 else:
-    for i in out_range: print (i)
+    for i in out_range: print i
